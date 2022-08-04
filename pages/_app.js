823f488cc1c9f356f255063e67527aced4e986e0
Mateus Sobreira <matheus.sobreira.27@gmail.com>
2022-08-03 21:59:23 -0300

    fixing title tag

--- pages/_app.js
+++ pages/_app.js
@@ -4,24 +4,21 @@ import { Layout } from '../components/index';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <title>Landing Page</title>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
-                <meta
-                    name="description"
-                    content="Landing page desenvolvida de modo a promover meu trabalho de freelancer. Mostrar que tenho conhecimentos de SEO, criatividade e capacidade. Bem-vindo."
-                />
-            </Head>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Mateus Codes® - Landing Page Frontend Mentor</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Landing page desenvolvida de modo a promover meu trabalho de freelancer. Mostrar que tenho conhecimentos de SEO, criatividade e capacidade. Bem-vindo."
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
 export default MyApp;
